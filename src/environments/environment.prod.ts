b96ca8bfef547600bd85c971f65008dbfd61fe10
Santiii02 <sir1003@alu.ubu.es>
2026-03-03 20:55:08 +0100

    Cambios para usar Vercel

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
 
-  apiBaseUrl: 'https://api.sportdb.dev',
+  apiBaseUrl: '',
   apiKey: 'SPe8I2ZQPzhIWkoGOKcq9gPjJcdEamywT7uUnw69',
   geminiApiKey: 'AIzaSyB_NuBpW6u6-TD57uZLhxBQORtjj88fsC8'
 
